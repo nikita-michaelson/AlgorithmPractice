6f5f01f107743a74ddf058f6675c047e7a63d1d4
nikita-michaelson <49412467+nikita-michaelson@users.noreply.github.com>
2022-07-26 19:13:12 -0400

    Time: 50 ms (76.11%), Space: 15.5 MB (10.09%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -9,12 +9,15 @@ class Solution(object):
         #first check if same length 
         if len(s) != len(t):
             return False
+
+        #two hashmap approach
+        #key = the letter, value = the number of occurences 
+        countS, countT = {}, {}
         
-        #sort both strings, if they are anagrams, they will be the same
-        
-        s = sorted(s)
-        t = sorted(t)
-        if s == t:
-            return True
-        else:
-            return False
+        for i in range(len(s)):
+            #make the key the letter and value the count , get returns the value
+            
+            countS[s[i]] = 1 + countS.get(s[i],0) 
+            countT[t[i]] = 1 + countT.get(t[i],0) 
+            
+        return countS == countT
