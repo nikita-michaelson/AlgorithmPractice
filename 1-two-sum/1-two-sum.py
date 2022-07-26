4f892dc9317e3448759cefc832410ce700f10cd8
nikita-michaelson <49412467+nikita-michaelson@users.noreply.github.com>
2022-07-26 19:45:39 -0400

    Time: 50 ms (88.64%), Space: 14.4 MB (44.39%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -4,10 +4,46 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        """
-        ## enumerate allows for an output of index then value
-        for i, num in enumerate(nums):
-            ## range produces numbers in sequence, index i+1 then stop at nums length
-            for j in range(i + 1, len(nums)):  
-                if num + nums[j] == target:
-                    return [i, j]
+        """  
+        hashmap = {}
+        for index,value in enumerate(nums):
+            #value as the key so we can utilize the has_key method
+            diff = target - value
+             
+            if diff in hashmap:
+                return [hashmap[diff], index]
+            hashmap[value] = index
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # ## enumerate allows for an output of index then value
+        # for i, num in enumerate(nums):
+        #     ## range produces numbers in sequence, index i+1 then stop at nums length
+        #     for j in range(i + 1, len(nums)):  
+        #         if num + nums[j] == target:
+        #             return [i, j]
